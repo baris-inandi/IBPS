e579e6c27afe178fdc23e23106678363210ae2d4
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2023-10-09 22:38:06 +0300

    ui

--- src/components/IDE/BottomBar/BottomBar.tsx
+++ src/components/IDE/BottomBar/BottomBar.tsx
@@ -1,10 +1,8 @@
 const BottomBar = () => {
   return (
-    <div className="text-white border-t dark:border-black py-[1px] px-3 text-sm w-full bg-blue-600 dark:bg-violet-500 flex justify-between">
-      <p>IBPS IDE v1.0.0 • IBPS Interpreter v2.0.0</p>
-      <p className="underline cursor-pointer">
-        Login/Register to Sync Your Files to the Cloud
-      </p>
+    <div className="text-white border-t dark:border-black px-3 text-xs w-full bg-blue-600 dark:bg-violet-600 flex justify-between">
+      <p>IB Pseudocode IDE v1.0.0</p>
+      <p>IB Pseudocode Interpreter v2.0.0</p>
     </div>
   );
 };
