c58be51397cc34ba473e9616c61fda1093e2c409
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2023-10-10 14:53:44 +0300

    ui

--- src/components/IDE/BottomBar/BottomBar.tsx
+++ src/components/IDE/BottomBar/BottomBar.tsx
@@ -1,6 +1,6 @@
 const BottomBar = () => {
   return (
-    <div className="text-white border-t dark:border-black px-3 text-xs w-full bg-blue-600 dark:bg-violet-600 flex justify-between">
+    <div className="font-medium text-white border-t dark:border-black px-3 text-xs w-full bg-blue-600 dark:bg-violet-500 flex justify-between">
       <p>IB Pseudocode IDE v1.0.0</p>
       <p>IB Pseudocode Interpreter v2.0.0</p>
     </div>
--- src/components/IDE/IDEPanels/ConsoleSectionTabs.tsx
+++ src/components/IDE/IDEPanels/ConsoleSectionTabs.tsx
@@ -1,12 +1,51 @@
+import { useAtom } from "jotai";
 import { SlTrash } from "react-icons/sl";
+import { activeRightPanelAtom } from "../../../atoms/atoms";
 
 const ConsoleSectionTabs = () => {
+  const [activeRightPanel, setActiveRightPanel] = useAtom(
+    activeRightPanelAtom,
+  );
+
   return (
-    <div className="flex w-full items-center justify-between">
-      <div className="flex gap-7 text-sm">
-        <button className="cursor-pointer">Ouptut</button>
-        <button className="cursor-pointer">REPL</button>
-        <button className="cursor-pointer">Reference</button>
+    <div className="flex w-full items-center justify-between h-full">
+      <div className="flex gap-1 text-sm h-full">
+        <button
+          onClick={() => setActiveRightPanel(0)}
+          className={`
+        cursor-pointer border-b-2 px-3 pt-1 ${
+          activeRightPanel === 0
+            ? "border-b-blue-600 dark:border-b-orange-400"
+            : "border-b-transparent"
+        }
+        `}
+        >
+          Ouptut
+        </button>
+        <button
+          onClick={() => setActiveRightPanel(1)}
+          className={`
+        cursor-pointer border-b-2 px-3 pt-1 ${
+          activeRightPanel === 1
+            ? "border-b-blue-600 dark:border-b-orange-400"
+            : "border-b-transparent"
+        }
+        `}
+        >
+          REPL
+        </button>
+        <button
+          onClick={() => setActiveRightPanel(2)}
+          className={`
+        cursor-pointer border-b-2 px-3 pt-1 ${
+          activeRightPanel === 2
+            ? "border-b-blue-600 dark:border-b-orange-400"
+            : "border-b-transparent"
+        }
+        `}
+        >
+          Reference
+        </button>
       </div>
       <div className="pr-1">
         <button className="flex justify-center items-center gap-2 p-1 text-red-700 dark:text-red-100 bg-red-700 dark:bg-red-600 dark:bg-opacity-20 bg-opacity-20 border border-red-700 dark:border-red-500 dark:border-opacity-20 border-opacity-20 rounded-md">
