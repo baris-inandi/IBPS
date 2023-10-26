59bfe9db96bddc58c79f4c2444cc6aa052db13d7
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2023-10-26 11:01:49 +0300

    stdlib bugfix

--- src/components/global/FileSwitchBar.tsx
+++ src/components/global/FileSwitchBar.tsx
@@ -1,4 +1,4 @@
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineFileText } from "react-icons/ai";
 import useFiles from "../../hooks/useFiles";
 
 const FileSwitchBar = () => {
@@ -6,8 +6,8 @@ const FileSwitchBar = () => {
 
   return (
     <div className="pr-10 h-full w-full">
-      <div className="flex items-center gap-2 h-full w-full max-w-sm bg-stone-200 rounded-md border border-stone-300 px-2 text-stone-600 dark:bg-onedark-800 dark:border-black dark:text-neutral-300">
-        <AiOutlineSearch />
+      <div className="flex items-center gap-2">
+        <AiOutlineFileText />
         <span>{activeFile}</span>
       </div>
     </div>
