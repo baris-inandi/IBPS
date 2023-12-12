48021b07157216d439a71b66ea5f28bba6741a6c
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2023-12-12 23:16:16 +0300

    speed insights fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Ide from "./components/Ide/Ide";
