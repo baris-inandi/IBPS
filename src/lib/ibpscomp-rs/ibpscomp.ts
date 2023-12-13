a967679f33152a09f154c0e5f9eaa2c37cec4f52
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2023-12-13 11:59:50 +0300

    better

--- src/lib/ibpscomp-rs/ibpscomp.ts
+++ src/lib/ibpscomp-rs/ibpscomp.ts
@@ -3,7 +3,8 @@ import init, { ibps_to_py, ibpscomp_rs_version } from "ibpscomp-rs";
 const ibpsToPy = async (code: string): Promise<string> => {
     await init();
     const out = ibps_to_py(code);
-    console.log(out);
+    const split = out.split("####");
+    console.log(split[split.length - 1]);
     return out;
 };
 
