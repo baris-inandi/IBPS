3d6bc1cd2461bc59005b241ee6e45a145b892ef5
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2024-01-17 17:40:42 +0300

    new array

--- ibpscomp-rs/stdlib/Array.py
+++ ibpscomp-rs/stdlib/Array.py
@@ -2,6 +2,10 @@ class Array(list):
     def __init__(self, *args):
         super().__init__([*args])
 
+    def push(self, *values):
+        for value in values:
+            self.append(value)
+
     @property
     def length(self):
         return len(self)
