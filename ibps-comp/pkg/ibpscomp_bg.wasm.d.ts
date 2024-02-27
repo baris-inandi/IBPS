57a6d3454d59226c6298d448d904b6f3dd47dcb7
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2024-02-28 01:13:59 +0300

    fixed.

---
+++ ibps-comp/pkg/ibpscomp_bg.wasm.d.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function ibps_to_py(a: number, b: number, c: number): void;
+export function compiler_version(a: number): void;
+export function ibpscomp_rs_version(a: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
